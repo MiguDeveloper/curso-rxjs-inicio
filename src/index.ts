68174d1eb4c6dd571a0ba4476e47b05449622c63
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-03-16 22:33:08 -0500

    observable basico

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+// Creamos un observable
+import {Observable} from 'rxjs';
 
+// const obs$ = Observable.create();
+const obs$ = new Observable(subs => {
+    subs.next('Hola');
+    subs.next('mundo');
 
-console.log('Hola Mundo!');
+    subs.complete();
+});
 
-
-
-
-
-
-
+obs$.subscribe(resp => console.log(resp));
